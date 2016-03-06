e4c742b9faa4124f7016ec1d2b9ffcd17d7adf49
Nikita Aghi <nikita.aghi@flixster-inc.com>
2016-03-05 17:20:23 -0800

    Fully working TodoApp in Angular.JS

---
+++ config/database.js
@@ -0,0 +1,7 @@
+/**
+ * Created by nikita on 3/5/16.
+ */
+module.exports = {
+    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl: 'mongodb://localhost/test'
+};
---
+++ public/core.js
@@ -0,0 +1,43 @@
+/**
+ * Created by nikita on 3/5/16.
+ */
+var scotchTodo = angular.module("scotchTodo", []);
+
+function mainController ($scope, $http) {
+    $scope.formData = {};
+
+    $http.get('/api/todos')
+        .success(function (data){
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data){
+            console.log("Error: " + data);
+        });
+
+    $scope.createTodo = function (){
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data){
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data){
+               console.log("Error: " + data);
+            });
+    };
+
+    $scope.deleteTodo = function (id){
+        $http.delete('/api/todos/' + id)
+            .success(function(data){
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data){
+                console.log("Error: " + data);
+            });
+
+
+    };
+
+}
